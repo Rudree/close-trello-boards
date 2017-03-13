539c46fd87d8144ab21e2d16df08e58593e3507d
Rudree Trivedi <rtrivedi@deakin.edu.au>
2017-03-13 12:19:40 +1100

    NEW: Get all boards ids

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 "use strict";
+var Trello = require('node-trello');
+var t = new Trello(process.env.TRELLO_KEY, process.env.TRELLO_TOKEN);
 
 module.exports = function() {
-  console.log("delete-trello-boards");
-};
+  t.get("/1/members/rudreetrivedi1", (error, body) => {
+    if(!error) {
+      console.log(body);
+    }
+  });
+}
